cf154b8a15b3d35d31d1e44fb00363f75e5a5929
Mark Dillon <mdillon@gmail.com>
2011-09-06 21:56:02 -0400

    clean up gemfile; add database_cleaner; bump version to 0.1.6

--- templates/gemfile.rb
+++ templates/gemfile.rb
@@ -35,6 +35,7 @@ group :test do
 end
 
 group :development, :test do
+  gem 'database_cleaner'
   gem "factory_girl_rails"
   gem "faker"
 end
--- templates/test_suite.rb
+++ templates/test_suite.rb
@@ -8,9 +8,17 @@ gsub_file 'spec/spec_helper.rb', /config\.fixture_path/, '# config.fixture_path'
 gsub_file 'spec/spec_helper.rb', /config\.use_transactional_fixtures/, '# config.use_transactional_fixtures'
 gsub_file 'spec/spec_helper.rb', /end/ do
 <<-RUBY
-  config.before :each do
-    Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+
+  # Clean up the database
+  require 'database_cleaner'
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.orm = "mongoid"
   end
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end
+  
 end
 RUBY
 end
